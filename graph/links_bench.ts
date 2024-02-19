36fc40e0133549d8958b5b0d47e02e51d5e9bedb
Nemo <nemo@daangn.com>
2024-02-19 16:56:00 +0900

    perf: benchmark `getDecls` stream vs array
    rimbu stream has no visible performance differences to array

---
+++ graph/links_bench.ts
@@ -0,0 +1,46 @@
+import { SourceFile } from "../deps/ts_morph.ts"
+import type { Declaration } from "./deps_map.ts"
+import { getDecls as getDeclsStream } from "./links.ts"
+import { getDeclExampleFile } from "./_example_project.ts"
+
+/**
+ * @module
+ *
+ * benchmark to show that Rimbu Streams aren't slower than
+ * array-based counterparts. (1.01x faster/slower than array)
+ *
+ * using Rimbu Streams everywhere removes conversion overhead
+ */
+
+const getDeclsArray =
+	(filter: (node: Declaration) => boolean) =>
+	(files: SourceFile[]): Declaration[] =>
+		files.flatMap((file) =>
+			[
+				...file.getVariableDeclarations(),
+				...file.getFunctions(),
+				...file.getClasses(),
+			].filter(filter)
+		)
+
+const filter = (node: Declaration) =>
+	!node.getName()?.toLowerCase().endsWith("ignore")
+
+for (const n of [1, 10, 100]) {
+	const group = `(x${n})`
+	Deno.bench(`array ${group}`, { group, baseline: true }, (b) => {
+		const files = getDeclExampleFile(n)
+
+		b.start()
+		const _decls = getDeclsArray(filter)([files])
+		b.end()
+	})
+
+	Deno.bench(`stream ${group}`, { group }, (b) => {
+		const files = getDeclExampleFile(n)
+
+		b.start()
+		const _decls = getDeclsStream(filter)([files]).toArray()
+		b.end()
+	})
+}
