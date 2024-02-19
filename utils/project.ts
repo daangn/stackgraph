ac76549eeb4ad6f6a7d070653d1bbffbab327b36
Nemo <nemo@daangn.com>
2024-02-19 16:48:41 +0900

    refactor: split out compiler option

--- utils/project.ts
+++ utils/project.ts
@@ -1,20 +1,27 @@
-import { ProjectOptions, ts } from "https://deno.land/x/ts_morph@21.0.1/mod.ts"
+import {
+	type CompilerOptions,
+	type ProjectOptions,
+	ts,
+} from "https://deno.land/x/ts_morph@21.0.1/mod.ts"
 import { FilteredFSHost } from "../graph/fs.ts"
 
 const ignore = /http|npm:|node_modules|\.jsx?|\.d\.ts/
+
+export const denoCompilerOptions: CompilerOptions = {
+	target: ts.ScriptTarget.ESNext,
+	module: ts.ModuleKind.ESNext,
+	moduleResolution: ts.ModuleResolutionKind.NodeNext,
+	strict: true,
+	allowJs: false,
+	checkJs: false,
+	allowImportingTsExtensions: true,
+	skipLibCheck: true,
+	types: [],
+	lib: [],
+}
+
 export const denoProjectOption: ProjectOptions = {
 	skipLoadingLibFiles: true,
-	compilerOptions: {
-		target: ts.ScriptTarget.ESNext,
-		module: ts.ModuleKind.ESNext,
-		moduleResolution: ts.ModuleResolutionKind.NodeNext,
-		strict: true,
-		allowJs: false,
-		checkJs: false,
-		allowImportingTsExtensions: true,
-		skipLibCheck: true,
-		types: [],
-		lib: [],
-	},
+	compilerOptions: denoCompilerOptions,
 	fileSystem: new FilteredFSHost((path) => ignore.test(path)),
 }
