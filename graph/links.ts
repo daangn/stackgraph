7d597449abbbe0008f0cb3f436bfefbc2332cfee
Nemo <nemo@daangn.com>
2024-02-19 11:04:49 +0900

    fix: do not store metadata for entrypoint nodes

--- graph/links.ts
+++ graph/links.ts
@@ -1,31 +1,14 @@
 import { SourceFile } from "../deps/ts_morph.ts"
 import { Declaration } from "./deps_map.ts"
 
-export type Metadata<A, B> = {
-	/**
-	 * Dependency not originated from module imports.
-	 * For example, `Redirect` in `<Redirect to="ComponentName" />`.
-	 */
-	links: A
-
-	/** All other data to query from given link */
-	metas: B
-}
-
-export type LinkedDecl<A, B> = Metadata<A, B> & { node: Declaration }
-
-/**
- * Filter and map links, metadatas, and  declaration
- */
-export type GetLink<A, B> = (node: Declaration) => LinkedDecl<A, B> | undefined
-
-export const generateLinks =
-	<A, B>(getLink: GetLink<A, B>) => (files: SourceFile[]): LinkedDecl<A, B>[] =>
+export type FilterEntryPoint = (node: Declaration) => boolean
+export const filterEntryPoints =
+	(filter: FilterEntryPoint) => (files: SourceFile[]): Declaration[] =>
 		files.flatMap((file) =>
 			[
 				...file.getVariableDeclarations(),
 				...file.getClasses(),
-			].flatMap((node) => getLink(node) ?? [])
+			].filter(filter)
 		)
 
 export const asMetadataRecord = <A, B>(links: LinkedDecl<A, B>[]) =>
