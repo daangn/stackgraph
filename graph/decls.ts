4002bc5fcbcb5dcee870e7f4d521d7fab76a2698
Nemo <nemo@daangn.com>
2024-02-20 15:38:28 +0900

    feat: getAllDecls

--- graph/decls.ts
+++ graph/decls.ts
@@ -6,20 +6,16 @@ import { SourceFile } from "../deps/ts_morph.ts"
 import { Declaration } from "./decl_deps.ts"
 
 /**
- * Find initial declarations to begin analyze with.
+ * Find initial top-level declarations to begin analyze with.
  */
 export const getDecls =
 	(filter: (node: Declaration) => boolean) =>
 	(files: StreamSource<SourceFile>): Stream<Declaration> =>
-		Stream.from(files).flatMap((file) =>
-			Stream.from<Declaration>(
-				file.getVariableDeclarations(),
-				file.getFunctions(),
-				file.getClasses(),
-			).filter(filter)
-		)
+		Stream.from(files).flatMap(getAllDecls).filter(filter)
 
-export const asMetadataRecord = <A, B>(links: LinkedDecl<A, B>[]) =>
-	Object.fromEntries(
-		links.map(({ node, ...metadata }) => [node.getName()!, metadata]),
+export const getAllDecls = (file: SourceFile): Stream<Declaration> =>
+	Stream.from<Declaration>(
+		file.getVariableDeclarations(),
+		file.getFunctions(),
+		file.getClasses(),
 	)
