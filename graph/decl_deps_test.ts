a58ccd99b9eed05d6fef863193c1ad20d216e8bc
Nemo <nemo@daangn.com>
2024-02-21 11:39:06 +0900

    test: same file reference in declDeps

--- graph/decl_deps_test.ts
+++ graph/decl_deps_test.ts
@@ -1,11 +1,21 @@
 import { inMemoryProject, withSrc } from "./_project.ts"
 import { exampleSrc } from "./_example_project.ts"
-import { assertSnapshot } from "../test_deps.ts"
+import { assertEquals, assertSnapshot } from "../test_deps.ts"
 import { Stream } from "https://deno.land/x/rimbu@1.2.0/stream/mod.ts"
 import { getAllDecls } from "./decls.ts"
 import { asRecord, declDepsSerializer, serializeNoColor } from "./_format.ts"
 import { snapshotTest } from "./_snapshot.ts"
 import { getDeclDeps } from "./decl_deps.ts"
+
+Deno.test("getDeclDeps() searches same file references for a", () => {
+	const project = inMemoryProject()
+	const a = project.createSourceFile("a.ts", exampleSrc["a.ts"])
+	const aDecl = a.getVariableDeclarationOrThrow("a")
+	const declDeps = getDeclDeps([aDecl])
+	const result = asRecord((x) => x.getName() ?? x.getText())(declDeps)
+
+	assertEquals(result, { a: new Set(["aaa"]), aaa: new Set([]) })
+})
 
 const project = inMemoryProject()
 const src = withSrc(project)(exampleSrc)
