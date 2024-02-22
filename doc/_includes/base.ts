ba672bc03039925d855411dda5c92456bee99819
Nemo <nemo@daangn.com>
2024-02-22 16:04:00 +0900

    refactor: extract to layout

---
+++ doc/_includes/base.ts
@@ -0,0 +1,14 @@
+export default ({ content, title, search }: Lume.Data, {}: Lume.Helpers) => {
+	console.log("pages:", search.pages())
+
+	return /*html*/ `
+    <!DOCTYPE html>
+    <head>
+        <meta charset="UTF-8">
+        <title>${title}</title>
+        <link rel="stylesheet" href="/assets/style.css" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.6.0/build/styles/github.min.css" />
+    </head>
+    <body>${content}</body>
+`
+}
--- doc/index.page.ts
+++ doc/index.page.ts
@@ -12,42 +12,6 @@ const header = /*md*/ `
 `
 
 export default async (_: Lume.Data, { md }: Lume.Helpers) => /*html*/ `
-    <!DOCTYPE html>
-
-    <head>
-        <meta charset="UTF-8">
-        <title>StackGraph</title>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.6.0/build/styles/github.min.css" />
-        <style>
-            body {
-                max-width: 1200px;
-                margin-inline: auto;
-            }
-            blockquote {
-                background-color: #f2e8da;
-                padding: 0.5em 1em;
-            }
-            pre {
-                border: 1px solid #e1e4e8;
-            }
-            code {
-                background-color: #dff4e6;
-            }
-            #graph {
-                height: 60vh;
-	            background-color: #f7fafc;
-            }
-
-            h2,
-            h3,
-            ul {
-                margin: 0.2em 0
-            }
-        </style>
-    </head>
-
-    <body>
-        ${md(header + await readme())}
-        <script type="module" src="./assets/demo.js"></script>
-    </body>
+    ${md(header + await readme())}
+    <script type="module" src="./assets/demo.js"></script>
 `
