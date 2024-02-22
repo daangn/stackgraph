efba5f71da28086a903ecd04374404a9cb5b6731
Nemo <nemo@daangn.com>
2024-02-22 19:02:51 +0900

    fix: add mobile viewport

--- doc/_includes/base.ts
+++ doc/_includes/base.ts
@@ -6,6 +6,7 @@ export default ({ content, title }: Lume.Data, {}: Lume.Helpers) => {
     <head>
         <meta charset="UTF-8">
         <title>${title}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="stylesheet" href="/assets/style.css" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.6.0/build/styles/github.min.css" />
     </head>
