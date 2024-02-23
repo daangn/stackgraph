16aa66a309a9835ed408f6e09a3e6f86349d89fb
Nemo <nemo@daangn.com>
2024-02-23 18:14:14 +0900

    fix: use relative URLs

--- _config.ts
+++ _config.ts
@@ -1,5 +1,6 @@
 import lume from "lume/mod.ts"
 import codeHighlight from "lume/plugins/code_highlight.ts"
+import relativeUrls from "lume/plugins/relative_urls.ts"
 import { codeAutoLink } from "./auto_link.ts"
 
 const site = lume({ src: "doc", prettyUrls: false })
@@ -9,5 +10,6 @@ site.copy("assets", "assets")
 site
 	.use(codeHighlight())
 	.use(codeAutoLink())
+	.use(relativeUrls())
 
 export default site
