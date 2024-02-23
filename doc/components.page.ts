bb20898c3e9743c58fcb0bbccb98f0f20bf6a225
Nemo <nemo@daangn.com>
2024-02-23 16:10:18 +0900

    docs: use actual code

--- doc/components.page.ts
+++ doc/components.page.ts
@@ -1,8 +1,8 @@
 import { Stream } from "https://deno.land/x/rimbu@1.2.0/stream/mod.ts"
 import { escapeHtml } from "https://deno.land/x/escape@1.4.2/mod.ts"
-export const title = "예제: 컴포넌트 관계도"
+import { exampleSrc } from "../graph/_example_project.ts"
 
-const { exampleSrc } = await import("../graph/_example_project.ts")
+export const title = "예제: 컴포넌트 관계도"
 
 export const codeBlock = (content: string) => /*html*/ `
     <pre><code class="language-ts hljs">${escapeHtml(content)}</code></pre>
@@ -23,7 +23,7 @@ export const head = /*html*/ `
             gap: 1rem;
             display: grid;
             align-items: center;
-            grid-template-columns: repeat(auto-fill, minmax(30rem, 1fr));
+            grid-template-columns: repeat(auto-fill, minmax(25rem, 1fr));
         }
         section {
             height: 100%;
@@ -31,8 +31,15 @@ export const head = /*html*/ `
     </style>
 `
 
+const code = await Deno.readTextFile(import.meta.dirname + "/components.ts")
+
 export default () => /*html*/ `
         <div id="graph" style="height:40vh" ></div>
         ${fileViewer}
+        <hr>
+        <section>
+            <h2>main.ts</h2>
+            ${codeBlock(code)}
+        </section>
         <script type="module" src="./assets/components.js" ></script>
     `
--- doc/components.ts
+++ doc/components.ts
@@ -1,20 +1,21 @@
 import { Stream } from "https://deno.land/x/rimbu@1.2.0/stream/mod.ts"
-import { inMemoryProject, withSrc } from "../graph/_project.ts"
-import { getAllDecls } from "../graph/decls.ts"
-import { getGraph } from "../graph/graph.ts"
-import { parseVSCodeURI } from "../graph/vscode_uri.ts"
-import { colorNode } from "./main.ts"
+import {
+	getAllDecls,
+	getGraph,
+	parseVSCodeURI,
+} from "https://raw.githubusercontent.com/daangn/stackgraph/main/graph/mod.ts"
+
 import { exampleSrc } from "../graph/_example_project.ts"
+import { colorNode } from "./main.ts"
+import { inMemoryProject, withSrc } from "../graph/_project.ts"
 
 const project = inMemoryProject()
 const files = withSrc(project)(exampleSrc)
+
 const decls = Stream.fromObjectValues(files).flatMap(getAllDecls).toArray()
 const graph = getGraph(decls)
 
-const nodes = graph.streamNodes()
-	.map(parseVSCodeURI)
-	.map(colorNode)
-	.toArray()
+const nodes = graph.streamNodes().map(parseVSCodeURI).map(colorNode).toArray()
 
 const links = graph.streamConnections()
 	.map(([source, target]) => ({
